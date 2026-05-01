a49e1d981ece46eb244ef0204fe883dd8d5ffeca
Albertovitale92 <albertovitale92@gmail.com>
2026-05-01 15:56:58 +0200

    Committing the new structure

--- risk_factors/transformations/__init__.py
+++ risk_factors/transformations/__init__.py
@@ -1,2 +1,45 @@
 """Data cleaning, normalization, and factor transformations."""
 
+from risk_factors.transformations.align import (
+    align_time_series,
+    align_to_common_index,
+)
+from risk_factors.transformations.correlations import (
+    correlation_matrix,
+    kendall_correlation,
+    pairwise_correlation,
+    partial_correlation,
+    pearson_correlation,
+    spearman_correlation,
+    tail_correlation,
+)
+from risk_factors.transformations.returns import (
+    abs_return,
+    cumulative_return,
+    log_return,
+    rel_returns,
+)
+from risk_factors.transformations.rolling import (
+    rolling_correlation,
+    rolling_mean,
+    rolling_volatility,
+)
+
+__all__ = [
+    "abs_return",
+    "align_time_series",
+    "align_to_common_index",
+    "correlation_matrix",
+    "cumulative_return",
+    "kendall_correlation",
+    "log_return",
+    "pairwise_correlation",
+    "partial_correlation",
+    "pearson_correlation",
+    "rel_returns",
+    "rolling_correlation",
+    "rolling_mean",
+    "rolling_volatility",
+    "spearman_correlation",
+    "tail_correlation",
+]
